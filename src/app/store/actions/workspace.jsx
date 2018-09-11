ebbee2f3b62bbcb55543e1a1280bd8b5a4564a27
iryna_barsukova <iryna_barsukova@epam.com>
2018-09-11 18:18:40 +0300

    Add redux

---
+++ src/app/store/actions/workspace.jsx
@@ -0,0 +1,9 @@
+export const setWorkspace = workspace => ({
+  type: 'SET_WORKSPACE',
+  workspace,
+})
+
+export const addWorkspace = workspace => ({
+  type: 'ADD_WORKSPACE',
+  workspace,
+})
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
 import './assets/styles/main.scss';
 
 import App from './app/components/app';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import store from './app/store';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
